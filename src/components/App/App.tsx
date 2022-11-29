aefdea0bff083e584702be396bc24ba76d2b0dd8
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-11-29 23:43:37 +0300

    chore: add other components but main plot

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Text from '../Text/Text';
 import './App.css';
 
 function App() {
   return (
     <div className="content">
       <Header />
+      <main>
+        <section className="diagram">Diagram</section>
+        <Text />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        <a href="https://github.com/novvember/mosmetro-tickets">
+          Репозиторий этого сайта на Github
+        </a>
+      </p>
+      <p className="footer__text">
+        © 2019–2022 <a href="https://github.com/novvember">novvember</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
