230feab19fa3355fa43bfed5edef06f5d365f5b4
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-12-04 13:33:38 +0300

    feat: divide ticket name if several tickets in tip

--- src/components/Tip/Tip.tsx
+++ src/components/Tip/Tip.tsx
@@ -3,17 +3,23 @@ import './Tip.css';
 
 function Tip({ cell }: { cell: Cell }) {
   const { metro, tat, minCost } = cell;
+  const names = minCost ? minCost.name.split('+').map((s) => s.trim()) : null;
 
   return (
     <div className="tip">
-      {minCost && (
-        <>
-          <p className="tip__ticket">{minCost.name}</p>
-          <p className="tip__cost">{minCost?.cost.toFixed(0)} ₽/месяц</p>
-        </>
-      )}
+      {names &&
+        names.map((name, i) => {
+          return (
+            <p key={i} className="tip__ticket">
+              {name}
+            </p>
+          );
+        })}
+
+      {names && <p className="tip__cost">{minCost?.cost.toFixed(0)} ₽/месяц</p>}
 
       <p className="tip__info">{metro} раз на метро</p>
+
       <p className="tip__info">{tat} раз на ТАТ</p>
     </div>
   );
