3fbd6a3e20c731a29e8934831426b51f4732bc8c
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-12-06 15:50:38 +0300

    style: change styles for header on small screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">Визуализатор проездных билетов Москвы</h1>
+      <h1 className="header__title">Визуализатор проездных билетов Москвы<sup className='header__version'>3.0</sup></h1>
       <p className="header__subtitle">
         Какой билет выбрать для регулярного использования? И сколько это будет
 стоить? Выберите на графике, сколько поездок на метро, а сколько
