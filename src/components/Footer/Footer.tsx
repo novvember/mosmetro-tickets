637f889c51cc4b7f90564d0476a592be75836d47
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-12-07 20:06:56 +0300

    content: update footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,11 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">
-        Сайт еще в разработке — дальше будет лучше. ⚹ Не имеет отношения
-        к «Московскому транспорту» ⚹{' '}
+        Не имеет отношения к «Московскому транспорту» /{' '}
         <a href="https://github.com/novvember/mosmetro-tickets">
           Репозиторий на Github
         </a>{' '}
-        ⚹ © 2019–2022 <a href="https://github.com/novvember">novvember</a>
+        / © 2019–2022 <a href="https://github.com/novvember">novvember</a>
       </p>
     </footer>
   );
