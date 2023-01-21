3fac523a7b3b35a099c1d56b9dd19bf4dd870444
Dmitry Kalugin <n.demitsuri@gmail.com>
2023-01-21 18:55:54 +0300

    content: change ver to 3.1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <nav id="menu-buttons" className='header__menu'></nav>
-      <h1 className="header__title">Визуализатор проездных билетов Москвы<sup className='header__version'>3.0</sup></h1>
+      <h1 className="header__title">Визуализатор проездных билетов Москвы<sup className='header__version'>3.1</sup></h1>
       <p className="header__subtitle">
         Какой билет выбрать для регулярного использования? И сколько это будет
 стоить? Выберите на графике, сколько поездок на метро, а сколько
